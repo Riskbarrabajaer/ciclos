88d58b5e0d09dfc67e2d0767f53aa844f4a03714
xavier <xesteban2772@gmail.com>
2024-04-17 13:45:56 -0400

    first commit

---
+++ src/main/java/com/mycompany/ciclos/Ciclos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ciclos;
+
+/**
+ *
+ * @author HP
+ */
+public class Ciclos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
